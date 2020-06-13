5d6b6c49bf92be49f906ed92d39f96105488a5a8
nashpotato <mncubenashe@gmail.com>
2020-06-13 15:27:06 +0100

    testing for disk persistence

--- test.cpp
+++ test.cpp
@@ -5,6 +5,7 @@
 #include <cassert>
 #include <cstring>
 #include <iostream>
+#include <fstream>
 #include "src/Table.h"
 #include "src/Repl.h"
 
@@ -32,8 +33,42 @@ void test_insert() {
     delete r;
     delete table;
 }
+
+void test_persistence(){
+    // Clear contents
+    std::ofstream f("./test_db", std::ofstream::trunc);
+    f.close();
+    auto *table = new Table("test_db");
+    std::vector<std::string> instructions;
+    instructions.emplace_back("insert 1 test test@email.com");
+    instructions.emplace_back("insert 2 test2 test2@email.com");
+    for(auto inst : instructions) {
+        Statement stmt;
+        Repl::stmtFromString(inst, stmt);
+        table->insert(stmt.tokens);
+    }
+    table->tableClose();
+    delete table;
+    auto table2 = new Table("test_db");
+
+    Row* r = new Row;
+    std::memcpy(r, table2->rowSlot(0), sizeof(Row));
+    assert(r->id == 1);
+    assert((Table::compareStringToChar(r->username, "test")));
+    assert((Table::compareStringToChar(r->email, "test@email.com")));
+
+    std::memcpy(r, table2->rowSlot(1), sizeof(Row));
+    assert(r->id == 2);
+    assert(Table::compareStringToChar(r->username, "test2"));
+    assert(Table::compareStringToChar(r->email, "test2@email.com"));
+
+    delete r;
+    delete table2;
+}
+
 int main() {
     test_insert();
+    test_persistence();
     std::cout << "All tests passed\n";
     return 0;
 }
